1d9e77f6bbdab9b4552e2a7175d7b13b7cb480ed
Denn Cayacap <denncayacap@gmail.com>
2025-08-31 20:49:42 +0800

    final dom exercise form and clear input

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
- const btn_add = document.getElementById("btn_add").addEventListener("click", addName)
-        const btn_clear = document.getElementById("btn_clear").addEventListener("click", clearList)
+const btn_add = document.getElementById("btn_add")
+const btn_clear = document.getElementById("btn_clear")
 
+btn_add.addEventListener("click", addName)
+btn_clear.addEventListener("click", clearList)
 
         function addName(){
-            let name_input = document.getElementById("id_name").value
-
+            let name_input = document.getElementById("id_name").value.trim()
+            const demo_li = document.getElementById("demo_li")
             if(name_input === ""){
                 alert("fill out all fields!")
                 return
@@ -15,11 +17,12 @@
             
             demo_li.append(newLi)
 
-            id_name.value = ""
+            name_input.value = ""
         }
             
         function clearList(){
-
+            
+            const demo_li = document.getElementById("demo_li")
             demo_li.textContent = "";
             
         }
